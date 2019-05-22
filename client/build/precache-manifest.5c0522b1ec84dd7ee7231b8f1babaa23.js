d5f5eb4b394b9e17ef6886600bc25ba5ea93d2b9
Brian Mullis <brian@smartrent.com>
2019-05-22 06:45:53 -0700

    Build react bundle and add to git

---
+++ client/build/precache-manifest.5c0522b1ec84dd7ee7231b8f1babaa23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00952c0f8003d9a150c258511d9caaf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af26356386cc897c6525",
+    "url": "/static/css/2.0ed265e4.chunk.css"
+  },
+  {
+    "revision": "65e63473179fd332e79f",
+    "url": "/static/css/main.8a06a192.chunk.css"
+  },
+  {
+    "revision": "af26356386cc897c6525",
+    "url": "/static/js/2.5e2ccca1.chunk.js"
+  },
+  {
+    "revision": "65e63473179fd332e79f",
+    "url": "/static/js/main.1221ecbf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
